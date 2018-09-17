4d9d927697ce19f515843564808ff99521aaa528
zcp <cpzhang2013@hotmail.com>
2018-09-17 22:33:49 +0800

    add tweened animation

---
+++ app/src/main/java/com/example/test/summarizeapp/fragment/ViewAnimationActivity.java
@@ -0,0 +1,47 @@
+package com.example.test.summarizeapp.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import com.example.test.summarizeapp.R;
+
+public class ViewAnimationActivity extends AppCompatActivity {
+
+    private Button btnTranslate;
+    private Button btnSet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veiw_animation);
+        initView();
+    }
+
+    private void initView() {
+        btnTranslate = findViewById(R.id.btn_view_ani);
+        btnTranslate.setOnClickListener(translateClickListener);
+        btnSet = findViewById(R.id.btn_view_set_ani);
+        btnSet.setOnClickListener(viewSetClickListener);
+    }
+
+    private View.OnClickListener translateClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Animation translateAnim = AnimationUtils.loadAnimation(ViewAnimationActivity.this, R.anim.translate_animation);
+            btnTranslate.startAnimation(translateAnim);
+        }
+    };
+
+    private View.OnClickListener viewSetClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Animation setAnimation = AnimationUtils.loadAnimation(ViewAnimationActivity.this, R.anim.view_set_animation);
+            btnSet.startAnimation(setAnimation);
+        }
+    };
+
+}
